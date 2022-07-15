3e651b58d32a1af6e90f774e605504ad68ce3caa
AntonioTS <vdeveleta96@gmail.com>
2022-07-15 22:07:02 +0200

    Subida dia 15/7

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interfaces';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,17 +11,23 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
-  buscar(){
+  buscar(termino: string){
     this.hayError = false;
+    this.termino = termino;
     console.log(this.termino);
+    
     this.paisService.buscarPais(this.termino)
       .subscribe((resp) => {
         console.log(resp);
+        this.paises = resp;
+
       }, (err) => {
         this.hayError = true;
+        this.paises = [];
       });
   }
 }
