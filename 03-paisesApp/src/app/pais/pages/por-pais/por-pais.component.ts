e8f8e7242efa7404e01095d0f8465aced20f0162
antonioTS <antonio24ts@gmail.com>
2022-07-19 03:50:40 +0200

    Fin de la app paisesApp

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,19 +4,29 @@ import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles: [
+    `
+      li {
+        cursor:pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisSugerido: Country[] = [];
+  mostrarSugeridos: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string){
     this.hayError = false;
     this.termino = termino;
+    this.mostrarSugeridos = false;
     console.log(this.termino);
     
     this.paisService.buscarPais(this.termino)
@@ -31,7 +41,11 @@ export class PorPaisComponent {
   }
 
   sugerencias(termino: string){
+    this.mostrarSugeridos = true;
+    this.termino = termino;
     this.hayError = false;
     // Crear sugerencias
+    this.paisService.buscarPais( termino )
+      .subscribe( pais => this.paisSugerido = pais.splice(0,5));
   }
 }
--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
-  templateUrl: './por-region.component.html'
+  templateUrl: './por-region.component.html',
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU','USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
+  regionActiva: string = '';
+  region: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region: string){
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
   }
 
+  activaRegion(region: string){
+
+    if(region === this.regionActiva) {return;}
+
+    this.regionActiva = region;
+
+    this.paisService.buscarRegion(region)
+      .subscribe(pais => {
+        console.log(pais);
+        this.region = pais;
+      })
+  }
 }
